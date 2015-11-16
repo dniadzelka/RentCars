a56959367faead6e719be13666ce9b69c6b141fe
Niadzelka Dzmitry <dniadzelka@exadel.by>
2015-11-16 16:32:56 +0300

    Directive: live loding pictures in add car page (beta); login forms

--- public/javascripts/controllers/addCarCtrl.js
+++ public/javascripts/controllers/addCarCtrl.js
@@ -1,7 +1,19 @@
 angular.module('addCarModule').controller('addCarCtrl', [
     '$scope',
     'cars',
-    function ($scope, cars) {
+    'fileReader',
+    function ($scope, cars, fileReader) {
+
+        $scope.getFile = function () {
+            $scope.progress = 0;
+            fileReader.readAsDataUrl($scope.file, $scope).then(function(result) {
+                $scope.imageSrc = result;
+            });
+        };
+
+        $scope.$on("fileProgress", function(e, progress) {
+            $scope.progress = progress.loaded / progress.total;
+        });
 
         $scope.addCar = function() {
 
