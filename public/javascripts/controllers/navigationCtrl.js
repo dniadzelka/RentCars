06879af4818c9731a75347845fa33b6280d2728c
Niadzelka Dzmitry <dniadzelka@exadel.by>
2015-11-19 12:37:06 +0300

    Add navigatinBar, footer (c)

--- public/javascripts/controllers/navigationCtrl.js
+++ public/javascripts/controllers/navigationCtrl.js
@@ -2,6 +2,7 @@ angular.module('rentCarsApp').controller('navigationCtrl', [
     '$scope',
     'auth',
     function ($scope, auth) {
+        $scope.navigation = {url: '/templates/navBar.ejs'};
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.currentUser = auth.currentUser;
         $scope.logOut = auth.logOut;
