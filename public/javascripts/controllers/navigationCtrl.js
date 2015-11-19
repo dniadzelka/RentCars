f2455e003420c5bfa611b3077a50c4ec81b5debb
Niadzelka Dzmitry <dniadzelka@exadel.by>
2015-11-19 14:48:37 +0300

    Add active class in addiction from .path in navigation

--- public/javascripts/controllers/navigationCtrl.js
+++ public/javascripts/controllers/navigationCtrl.js
@@ -1,10 +1,17 @@
 angular.module('rentCarsApp').controller('navigationCtrl', [
     '$scope',
+    '$location',
     'auth',
-    function ($scope, auth) {
+    function ($scope, $location, auth) {
         $scope.navigation = {url: '/templates/navBar.ejs'};
         $scope.isLoggedIn = auth.isLoggedIn;
         $scope.currentUser = auth.currentUser;
         $scope.logOut = auth.logOut;
+
+        $scope.isActive = function (viewLocation) {
+            var result = (viewLocation === $location.path());
+            return result;
+        };
+
     }
 ]);
