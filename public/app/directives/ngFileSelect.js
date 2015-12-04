9a4fecc8f1dd2603bec14c57f3f1575421e4f1d9
culogordo <dniadzelka@exadel.by>
2015-12-04 11:53:59 +0300

    remove console logs

--- public/app/directives/ngFileSelect.js
+++ public/app/directives/ngFileSelect.js
@@ -4,7 +4,6 @@ angular.module('rentCarsApp').directive('ngFileSelect', function() {
 
             el.bind('change', function(e) {
                 $scope.file = e.target.files[0];
-                console.log($scope.file);
                 $scope.getFile();
             })
 
--- public/production/production.js
+++ public/production/production.js
@@ -163,7 +163,6 @@ angular.module('rentCarsApp').directive('ngFileSelect', function() {
 
             el.bind('change', function(e) {
                 $scope.file = e.target.files[0];
-                console.log($scope.file);
                 $scope.getFile();
             })
 
