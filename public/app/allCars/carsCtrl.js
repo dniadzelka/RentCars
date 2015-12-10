a594107d2d78167f6c25ffa986b12dd8e215c719
culogordo <dniadzelka@exadel.by>
2015-12-10 14:32:41 +0300

    redisign cars

--- public/app/allCars/carsCtrl.js
+++ public/app/allCars/carsCtrl.js
@@ -1,7 +1,11 @@
 angular.module('carsModule').controller('carsCtrl', [
     '$scope',
+    '$location',
     'cars',
-    function ($scope, cars) {
+    function ($scope, $location, cars) {
         $scope.cars = cars.cars;
+        $scope.toAboutCarPage = function (id) {
+            $location.path('/cars/aboutCar_' + id);
+        }
     }
 ]);
--- public/production/production.js
+++ public/production/production.js
@@ -663,9 +663,13 @@ angular.module('rentCarsApp').controller('navigationCtrl', [
 
 angular.module('carsModule').controller('carsCtrl', [
     '$scope',
+    '$location',
     'cars',
-    function ($scope, cars) {
+    function ($scope, $location, cars) {
         $scope.cars = cars.cars;
+        $scope.toAboutCarPage = function (id) {
+            $location.path('/cars/aboutCar_' + id);
+        }
     }
 ]);
 
