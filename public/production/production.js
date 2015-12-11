bcc08550cb12f8db416180113a3084c12198e4f6
culogordo <dniadzelka@exadel.by>
2015-12-11 18:42:09 +0300

    change to (ready) in ngEllipsis.js

--- public/production/production.js
+++ public/production/production.js
@@ -137,52 +137,19 @@ feedbacksModule.config([
     }
 ]);
 
-angular.module('rentCarsApp').directive('ngEllipsis', [function () {
+angular.module('rentCarsApp').directive('ngEllipsis', ['$document',
+    function ($document) {
         return {
             restrict: 'A',
+            scope: true,
             link: function () {
-
                 /**
                 * Directive 'ngEllipsis' is used to customize ellipsis in overflow-text paragraph.
                 */
-
-                $(document).ready(function() {
-                	$(".feedbackTextWrapper").dotdotdot({
-                            /*	The text to add as ellipsis. */
-		                    ellipsis : '... ',
-
-                    		/*	How to cut off the text/html: 'word'/'letter'/'children' */
-                    		wrap : 'word',
-
-                    		/*	Wrap-option fallback to 'letter' for long words */
-                    		fallbackToLetter: true,
-
-                    		/*	jQuery-selector for the element to keep and put after the ellipsis. */
-                    		after : null,
-
-                    		/*	Whether to update the ellipsis: true/'window' */
-                    		watch : true,
-
-                    		/*	Optionally set a max-height, if null, the height will be measured. */
-                    		height : null,
-
-                    		/*	Deviation for the height-option. */
-                    		tolerance : 0,
-
-                    		/*	Callback function that is fired after the ellipsis is added,
-                    			receives two parameters: isTruncated(boolean), orgContent(string). */
-                    		callback : function( isTruncated, orgContent ) {},
-
-                    		lastCharacter : {
-
-                    			/*	Remove these characters from the end of the truncated text. */
-                    			remove : [ ' ', ',', ';', '.', '!', '?' ],
-
-                    			/*	Don't add an ellipsis if this array contains
-                    				the last character of the truncated text. */
-                    			noEllipsis	: []
-                    		}
-                    });
+                $document.ready(function() {
+                	$(".feedbackTextWrapper").dotdotdot(
+                        {   watch : true	}
+                    );
                 });
             }
         };
